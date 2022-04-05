a0e65bce952cc82682f18aee20eea6fe243ffd0d
Edwin Bautista <edwin.bautista@glocation.com.co>
2022-04-05 17:10:36 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const { exec } = require('child_process');
+const app = express()
+const port = 3000
+
+app.get('/setLayer', (req, res) => {
+    let { clientId, fileName } = req.body;
+    exec(`mkdir -p /usr/share/geoserver/data_dir/client_sources/${clientId}/ && gsutil cp gs://geoviz/clients/${clientId}/${fileName} /usr/share/geoserver/data_dir/client_sources/${clientId}/`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            res.send('Error');
+        }
+        console.log(`exec stdout: ${stdout}`);
+        res.send('Success');
+    });
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})

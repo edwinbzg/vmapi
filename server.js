06f3d73ca231e3582517208dc4aa81355603798e
Edwin Bautista <edwin.bautista@glocation.com.co>
2022-04-06 10:16:40 -0500

    test

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { exec } = require('child_process');
+const { exec, execSync } = require('child_process');
 const axios = require('axios')
 const app = express()
 const port = 3000
@@ -12,14 +12,25 @@ app.get('/setLayer', async (req, res) => {
     const name = fileName.split('.').slice(0, -1).join('.')
 
     // Download GeoJSON
-    await exec.__promisify__(`mkdir -p /usr/share/geoserver/data_dir/client_sources/${clientId}/ && gsutil cp gs://geoviz/clients/${clientId}/geojson/${fileName} /usr/share/geoserver/data_dir/client_sources/${clientId}/`).catch(e => {
-        console.log(e);
-    });
+    exec(`mkdir -p /usr/share/geoserver/data_dir/client_sources/${clientId}/ && gsutil cp gs://geoviz/clients/${clientId}/geojson/${fileName} /usr/share/geoserver/data_dir/client_sources/${clientId}/`, (error, stdout, stderr) => {
+        if (!error) {
+            // Convert to GeoPackage
+            exec(`ogr2ogr -f GPKG ${name}.gpkg /usr/share/geoserver/data_dir/client_sources/${clientId}/${fileName} -lco GEOMETRY_NAME=geom -lco OVERWRITE=YES -a_srs 'EPSG:4326'`, (error, stdout, stderr) => {
+                if (!error) {
+                    res.send('ok')
+                } else {
+                    console.log(stderr);
+                    res.send(stderr)
+                }
+            });
+        } else {
+            console.log(stderr);
+            res.send(stderr)
+        }
+    })
 
-    // Convert to GeoPackage
-    await exec.__promisify__(`ogr2ogr -f GPKG ${name}.gpkg /usr/share/geoserver/data_dir/client_sources/${clientId}/${fileName} -lco GEOMETRY_NAME=geom -lco OVERWRITE=YES -a_srs 'EPSG:4326'`).catch(e => {
-        console.log(e);
-    });
+
+
     // console.log(`ogr2ogr -f GPKG ${name}.gpkg /usr/share/geoserver/data_dir/client_sources/${clientId}/${fileName} -lco GEOMETRY_NAME=geom -lco OVERWRITE=YES -a_srs 'EPSG:4326'`)
     // Create datastore
     // axios.post('http://localhost:8080/geoserver/rest/workspaces/clients/datastores', {

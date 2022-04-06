6e7fa5a78a38a2cd675815ff2b876bb0a443d4d1
Edwin Bautista <edwin.bautista@glocation.com.co>
2022-04-06 08:23:50 -0500

    test create

--- server.js
+++ server.js
@@ -8,14 +8,38 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get('/setLayer', (req, res) => {
     let { clientId, fileName } = req.query;
-    exec(`mkdir -p /usr/share/geoserver/data_dir/client_sources/${clientId}/ && gsutil cp gs://geoviz/clients/${clientId}/geojson/${fileName} /usr/share/geoserver/data_dir/client_sources/${clientId}/`, (error, stdout, stderr) => {
+    const name = fileName.split('.').slice(0, -1).join('.')
+
+    // Download GeoJSON
+    exec(`mkdir -p /usr/share/geoserver/data_dir/client_sources/${clientId}/ && gsutil cp gs://geoviz/clients/${clientId}/geojson/${fileName} /usr/share/geoserver/data_dir/client_sources/${clientId}/`, execOutput);
+    // Convert to GeoPackage
+    exec(`ogr2ogr -f GPKG test.gpkg geojson-1649194423919.geojson -lco GEOMETRY_NAME=geom -lco OVERWRITE=YES -a_srs 'EPSG:4326'`, execOutput)
+    // Create datastore
+    var create = request({
+        url: 'http://localhost:8080/geoserver/rest/workspaces/clients/datastores',
+        method: 'POST',
+        form: {
+            "dataStore": {
+                "name": name,
+                "connectionParameters": {
+                    "entry": [
+                        { "@key": "database", "$": `file:client_sources/2/${fileName}` },
+                        { "@key": "dbtype", "$": "geopkg" }
+                    ]
+                }
+            }
+        }
+    }, function (error, response, body) {
+        res.json(global.response(true, 1, 'Se ha actualizado la configuración correctamente.'));
+    });
+
+    function execOutput(error, stdout, stderr) {
         if (error) {
             console.error(`exec error: ${error}`);
             res.send('Error');
         }
-        console.log(`exec stdout: ${stdout}`);
-        res.send('Success');
-    });
+        // res.send('Success');
+    }
 })
 
 app.listen(port, () => {

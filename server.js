85d87e05ffe7ac22945f2440517bf393e8b7fb9b
Edwin Bautista <edwin.bautista@glocation.com.co>
2022-04-06 09:49:37 -0500

    test

--- server.js
+++ server.js
@@ -17,28 +17,27 @@ app.get('/setLayer', (req, res) => {
     exec(`ogr2ogr -f GPKG ${name}.gpkg /usr/share/geoserver/data_dir/client_sources/${clientId}/${fileName} -lco GEOMETRY_NAME=geom -lco OVERWRITE=YES -a_srs 'EPSG:4326'`, execOutput)
     // console.log(`ogr2ogr -f GPKG ${name}.gpkg /usr/share/geoserver/data_dir/client_sources/${clientId}/${fileName} -lco GEOMETRY_NAME=geom -lco OVERWRITE=YES -a_srs 'EPSG:4326'`)
     // Create datastore
-    axios.post('http://localhost:8080/geoserver/rest/workspaces/clients/datastores', {
-        "dataStore": {
-            "name": name,
-            "connectionParameters": {
-                "entry": [
-                    { "@key": "database", "$": `file:client_sources/2/${name}.gpkg` },
-                    { "@key": "dbtype", "$": "geopkg" }
-                ]
-            }
-        }
-    })
-        .then(resp => {
-            console.log(`statusCode: ${resp.status}`)
-            console.log(resp)
-            res.send(resp)
-        })
-        .catch(error => {
-            console.error(error)
-            res.send(error)
-        })
+    // axios.post('http://localhost:8080/geoserver/rest/workspaces/clients/datastores', {
+    //     "dataStore": {
+    //         "name": name,
+    //         "connectionParameters": {
+    //             "entry": [
+    //                 { "@key": "database", "$": `file:client_sources/2/${name}.gpkg` },
+    //                 { "@key": "dbtype", "$": "geopkg" }
+    //             ]
+    //         }
+    //     }
+    // }).then(resp => {
+    //         console.log(`statusCode: ${resp.status}`)
+    //         console.log(resp)
+    //         res.send(resp)
+    //     })
+    //     .catch(error => {
+    //         console.error(error)
+    //         res.send(error)
+    //     })
 
-        
+
     function execOutput(error, stdout, stderr) {
         if (error) {
             console.error(`exec error: ${error}`);

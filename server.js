ab49e7ab7d8016984a89aa1e2755fa1cbc048ed7
Edwin Bautista <edwin.bautista@glocation.com.co>
2022-04-06 11:15:27 -0500

    test

--- server.js
+++ server.js
@@ -32,86 +32,120 @@ app.get('/setLayer', async (req, res) => {
                         console.log(`statusCode: ${resp.status}`)
                         if (resp.status == 201) {
                             // Create layer
-                            axios.post(`http://localhost:8080/geoserver/rest/workspaces/clients/datastores/${name}/featuretypes?recalculate=nativebbox,latlonbbox`,
+                            axios.post(`http://localhost:8080/geoserver/rest/workspaces/clients/datastores/${name}/featuretypes`,
                                 {
-                                    "name": name,
-                                    "nativeName": name,
+                                    "name": "geojson-1649194423919",
+                                    "nativeName": "geojson-1649194423919",
                                     "namespace": {
                                         "name": "clients",
                                         "href": "http://localhost:8080/geoserver/rest/namespaces/clients.json"
                                     },
-                                    "title": name,
-                                    "abstract": "",
+                                    "title": "geojson-1649194423919",
                                     "keywords": {
                                         "string": [
-                                            name,
+                                            "features",
+                                            "geojson-1649194423919"
                                         ]
                                     },
-                                    "metadataLinks": {
-                                        "metadataLink": []
-                                    },
-                                    "dataLinks": {
-                                        "org.geoserver.catalog.impl.DataLinkInfoImpl": []
-                                    },
-                                    "nativeCRS": "EPSG:4326",
+                                    "nativeCRS": "GEOGCS[\"WGS 84\", \n  DATUM[\"World Geodetic System 1984\", \n    SPHEROID[\"WGS 84\", 6378137.0, 298.257223563, AUTHORITY[\"EPSG\",\"7030\"]], \n    AUTHORITY[\"EPSG\",\"6326\"]], \n  PRIMEM[\"Greenwich\", 0.0, AUTHORITY[\"EPSG\",\"8901\"]], \n  UNIT[\"degree\", 0.017453292519943295], \n  AXIS[\"Geodetic longitude\", EAST], \n  AXIS[\"Geodetic latitude\", NORTH], \n  AUTHORITY[\"EPSG\",\"4326\"]]",
                                     "srs": "EPSG:4326",
                                     "nativeBoundingBox": {
-                                        "minx": 0,
-                                        "maxx": 0,
-                                        "miny": 0,
-                                        "maxy": 0,
+                                        "minx": -74.2652,
+                                        "maxx": -74.2414,
+                                        "miny": 4.48058,
+                                        "maxy": 4.50669,
                                         "crs": "EPSG:4326"
                                     },
                                     "latLonBoundingBox": {
-                                        "minx": 0,
-                                        "maxx": 0,
-                                        "miny": 0,
-                                        "maxy": 0,
+                                        "minx": -74.2652,
+                                        "maxx": -74.2414,
+                                        "miny": 4.48058,
+                                        "maxy": 4.50669,
                                         "crs": "EPSG:4326"
                                     },
-                                    "projectionPolicy": "REPROJECT_TO_DECLARED",
+                                    "projectionPolicy": "FORCE_DECLARED",
                                     "enabled": true,
-                                    "metadata": {
-                                        "entry": [
-                                            
-                                        ]
-                                    },
                                     "store": {
                                         "@class": "dataStore",
-                                        "name": "clients:" + name,
-                                        "href": "http://localhost:8080/geoserver/rest/workspaces/clients/datastores/" + name + ".json"
+                                        "name": "clients:geojson-1649194423919",
+                                        "href": "http://localhost:8080/geoserver/rest/workspaces/clients/datastores/geojson-1649194423919.json"
                                     },
-                                    "cqlFilter": "INCLUDE",
-                                    "maxFeatures": 100,
-                                    "numDecimals": 6,
-                                    "responseSRS": {
-                                        "string": [
-                                            4326
-                                        ]
-                                    },
-                                    "overridingServiceSRS": true,
-                                    "skipNumberMatched": true,
-                                    "circularArcPresent": true,
-                                    "linearizationTolerance": 10,
+                                    "serviceConfiguration": false,
+                                    "simpleConversionEnabled": false,
+                                    "internationalTitle": "",
+                                    "internationalAbstract": "",
+                                    "maxFeatures": 0,
+                                    "numDecimals": 0,
+                                    "padWithZeros": false,
+                                    "forcedDecimal": false,
+                                    "overridingServiceSRS": false,
+                                    "skipNumberMatched": false,
+                                    "circularArcPresent": false,
                                     "attributes": {
                                         "attribute": [
                                             {
-                                                "name": "the_geom",
+                                                "name": "geom",
                                                 "minOccurs": 0,
                                                 "maxOccurs": 1,
                                                 "nillable": true,
-                                                "binding": "org.locationtech.jts.geom.Point"
+                                                "binding": "org.locationtech.jts.geom.Polygon"
                                             },
-                                            {},
-                                            {},
-                                            {}
+                                            {
+                                                "name": "stroke",
+                                                "minOccurs": 0,
+                                                "maxOccurs": 1,
+                                                "nillable": true,
+                                                "binding": "java.lang.String"
+                                            },
+                                            {
+                                                "name": "stroke-width",
+                                                "minOccurs": 0,
+                                                "maxOccurs": 1,
+                                                "nillable": true,
+                                                "binding": "java.lang.Integer"
+                                            },
+                                            {
+                                                "name": "stroke-opacity",
+                                                "minOccurs": 0,
+                                                "maxOccurs": 1,
+                                                "nillable": true,
+                                                "binding": "java.lang.Integer"
+                                            },
+                                            {
+                                                "name": "fill",
+                                                "minOccurs": 0,
+                                                "maxOccurs": 1,
+                                                "nillable": true,
+                                                "binding": "java.lang.String"
+                                            },
+                                            {
+                                                "name": "fill-opacity",
+                                                "minOccurs": 0,
+                                                "maxOccurs": 1,
+                                                "nillable": true,
+                                                "binding": "java.lang.Double"
+                                            },
+                                            {
+                                                "name": "Name",
+                                                "minOccurs": 0,
+                                                "maxOccurs": 1,
+                                                "nillable": true,
+                                                "binding": "java.lang.String"
+                                            },
+                                            {
+                                                "name": "description",
+                                                "minOccurs": 0,
+                                                "maxOccurs": 1,
+                                                "nillable": true,
+                                                "binding": "java.lang.String"
+                                            }
                                         ]
                                     }
-                                }).then((resp2)=>{
+                                }).then((resp2) => {
                                     console.log(resp2);
                                     res.sendStatus(resp2.status);
 
-                                }).catch(error=>{
+                                }).catch(error => {
                                     console.error(error)
                                     res.send(error)
                                 });

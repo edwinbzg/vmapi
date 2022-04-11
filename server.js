24fb860bca0819c4b177d42a804c0a7ca0726b3d
Edwin Bautista <edwin.bautista@glocation.com.co>
2022-04-11 17:30:34 -0500

    update sql

--- server.js
+++ server.js
@@ -178,7 +178,7 @@ app.get('/delGeoJSONLayer', async (req, res) => {
 });
 
 app.get('setGeoPackageLayer', async (req, res) => {
-    let { clientId, fileName } = req.query;
+    let { clientId, fileName, table } = req.query;
     const name = fileName.split('.').slice(0, -1).join('.')
 
     // Download GeoJSON
@@ -199,7 +199,7 @@ app.get('setGeoPackageLayer', async (req, res) => {
                 console.log(`statusCode: ${resp.status}`)
                 if (resp.status == 201) {
                     // Get Columns
-                    exec(`ogrinfo /usr/share/geoserver/data_dir/client_sources/${clientId}/${name}.gpkg -sql "SELECT json_group_array(json_object('cid', cid,'name', name,'type', type,'dflt_value', dflt_value,'pk', pk)) AS json_result FROM (SELECT * FROM pragma_table_info('${name}'))"`, (error, stdout, stderr) => {
+                    exec(`ogrinfo /usr/share/geoserver/data_dir/client_sources/${clientId}/${name}.gpkg -sql "SELECT json_group_array(json_object('cid', cid,'name', name,'type', type,'dflt_value', dflt_value,'pk', pk)) AS json_result FROM (SELECT * FROM pragma_table_info('${table}'))"`, (error, stdout, stderr) => {
                         if (!error) {
                             var columns = JSON.parse(stdout.split('json_result (String) = ')[1]);
                             var attributes = [];
